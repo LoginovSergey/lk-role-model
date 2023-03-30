c7c4ff77994a45e2b4f1bea4bfa91d8db417ab77
Sergey <gigabait1221@yandex.ru>
2023-03-30 23:15:07 +0300

    add features

--- src/app/modules/cabinet/cabinet-routing.module.ts
+++ src/app/modules/cabinet/cabinet-routing.module.ts
@@ -2,8 +2,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { CabinetResolver } from './providers/cabinet.resolver';
-import { CabinetTodosGuard } from './providers/cabinet-todos.guard';
-import { CabinetAdminGuard } from './providers/cabinet-admin.guard';
 
 import { LayoutComponent } from './layout/layout.component';
 
@@ -21,12 +19,10 @@ const routes: Routes = [
       {
         path: 'todos',
         loadChildren: () => import('./modules/todos/todos.module').then(m => m.TodosModule),
-        canLoad: [CabinetTodosGuard],
       },
       {
         path: 'admin',
         loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule),
-        canLoad: [CabinetAdminGuard],
       },
     ],
   },
