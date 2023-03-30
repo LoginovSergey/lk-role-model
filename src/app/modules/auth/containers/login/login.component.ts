a769465bf5a936b71bfd9cb45e4d6bd78fdc7ba8
Sergey <gigabait1221@yandex.ru>
2023-03-30 22:30:25 +0300

    fix

--- src/app/modules/auth/containers/login/login.component.ts
+++ src/app/modules/auth/containers/login/login.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Subscription } from 'rxjs';
+import { Subject, takeUntil } from 'rxjs';
 
 import { AuthService } from '../../services/auth.service';
 
@@ -15,28 +15,36 @@ import { LoginFormModel } from '../../models/login-form.model';
 })
 
 export class LoginComponent implements OnDestroy {
-  subscriptions$: Subscription = new Subscription();
 
   form: FormGroup = new FormGroup({
-    phone: new FormControl('', [Validators.required, Validators.pattern('^\\+?[78][-\\(]?\\d{3}\\)?-?\\d{3}-?\\d{2}-?\\d{2}$'), Validators.minLength(11), Validators.maxLength(11)]),
+    phone: new FormControl('', [Validators.required, Validators.minLength(11), Validators.maxLength(11)]),
     password: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(10)]),
   })
 
-  constructor(private service: AuthService,
-              private router: Router) { }
+  private destroy$ = new Subject<void>()
+
+  constructor(
+    private service: AuthService,
+    private router: Router
+  ) { }
 
   login(formValue: LoginFormModel): void {
     this.form.disable();
-    this.subscriptions$.add(this.service.login(formValue).subscribe(res => {
+    this.service.login(formValue)
+      .pipe(
+        takeUntil(this.destroy$)
+      )
+      .subscribe(res => {
       this.form.enable();
       sessionStorage.setItem('lk-role-model', 'authenticated-token');
       sessionStorage.setItem('lk-role-model__role', res);
       this.router.navigate(['/cabinet']);
-    }))
+    })
   }
 
   ngOnDestroy(): void {
-    this.subscriptions$.unsubscribe()
+    this.destroy$.next()
+    this.destroy$.complete()
   }
 
 }
